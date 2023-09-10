cec0ab5109237d52c285f7f8f8990e5510be4ef4
michael7nightingale <suslanchikmopl@gmail.com>
2023-09-10 13:15:59 +0500

    creat base static config

--- src/app.py
+++ src/app.py
@@ -1,5 +1,24 @@
 from litestar import Litestar
+from litestar.template.config import TemplateConfig
+from litestar.contrib.jinja import JinjaTemplateEngine
+from litestar.static_files import StaticFilesConfig
+
+from pathlib import Path
+
 from internal.routes import route_handlers
 
 
-create_app = Litestar(route_handlers=route_handlers)
+create_app = Litestar(
+    route_handlers=route_handlers,
+    template_config=TemplateConfig(
+        directory=Path("internal/templates"),
+        engine=JinjaTemplateEngine
+    ),
+    static_files_config=[
+        StaticFilesConfig(
+            directories=['internal/static'],
+            path="/static"
+        )
+    ],
+
+)
