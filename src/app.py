fa4f64142c3332d07e17c2fb159cc511c5b6d58a
michael7nightingale <suslanchikmopl@gmail.com>
2023-09-10 15:02:36 +0500

    add app class

--- src/app.py
+++ src/app.py
@@ -1,24 +1,7 @@
 from litestar import Litestar
-from litestar.template.config import TemplateConfig
-from litestar.contrib.jinja import JinjaTemplateEngine
-from litestar.static_files import StaticFilesConfig
 
-from pathlib import Path
-
-from internal.routes import route_handlers
+from internal.core.app import App
 
 
-create_app = Litestar(
-    route_handlers=route_handlers,
-    template_config=TemplateConfig(
-        directory=Path("internal/templates"),
-        engine=JinjaTemplateEngine
-    ),
-    static_files_config=[
-        StaticFilesConfig(
-            directories=['internal/static'],
-            path="/static"
-        )
-    ],
-
-)
+def create_app() -> Litestar:
+    return App().app
