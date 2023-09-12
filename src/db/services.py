f60c3067e2eeb7edb8b5fc4833c33279cc4cc14c
michael7nightingale <suslanchikmopl@gmail.com>
2023-09-12 16:44:35 +0500

    create table reservation db service

--- src/db/services.py
+++ src/db/services.py
@@ -1,7 +1,9 @@
-from .tables import Ingredient, Product, Category
+import datetime
+
+from .tables import Ingredient, Product, Category, TableReservation
 
 
-async def get_all_categories() -> dict:
+async def get_all_categories() -> list[dict]:
     return await Category.select(Category.name, Category.slug)
 
 
@@ -9,10 +11,11 @@ async def get_category_by_slug(category_slug: str) -> dict:
     return await (
         Category.select(Category.all_columns())
         .where(Category.slug == category_slug)
+        .first()
     )
 
 
-async def get_products_by_category_slug(category_slug: str) -> dict:
+async def get_products_by_category_slug(category_slug: str) -> list[dict]:
     return await (
         Product.select(
                 Product.all_columns(),
@@ -34,4 +37,22 @@ async def get_product_with_ingredients_by_slug(category_slug, product_slug: str)
                 Product.category.slug == category_slug
             )
         .first()
+    )
+
+
+async def create_table_reservation(
+    name: str,
+    phone: str,
+    number_of_guests: int,
+    date: datetime.date,
+    message: str
+) -> TableReservation:
+    return await TableReservation.insert(
+        TableReservation(
+            name=name,
+            phone=phone,
+            number_of_guests=number_of_guests,
+            date=date,
+            message=message
+        )
     )
